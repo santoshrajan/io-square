bf321433584bc63550e3847d4c26042365368f8a
Santosh Rajan <santrajan@gmail.com>
2016-08-31 17:49:14 +0530

    adds bind

--- test/test.js
+++ test/test.js
@@ -1,15 +1,19 @@
 
 const IO = require('../lib/io-square')
+const fs = require('fs')
 
-new IO()
-  .readFile('test/test.js')
-  .reject(data => {
-    if (data.error) {
-      console.log('error')
-      return true
-    } else {
-      return false
-    }
+new IO(next => {
+  fs.readFile('test/test.js', (error, data) => {
+    next({error: error, data: data})
   })
-  .map(data => data.data.toString())
-  .next(data => console.log(data))
+})
+.reject(data => {
+  if (data.error) {
+    console.log('error')
+    return true
+  } else {
+    return false
+  }
+})
+.map(data => data.data.toString())
+.next(data => console.log(data))
