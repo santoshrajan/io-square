2f3129a5294331c73bc41b583c140b011b223486
Santosh Rajan <santrajan@gmail.com>
2016-09-01 12:46:22 +0530

    adds repo

--- test/test.js
+++ test/test.js
@@ -2,21 +2,19 @@
 const IO = require('../lib/io-square')
 const fs = require('fs')
 
-new IO(next => {
-  fs.readFile('test/test.js', next)
-})
-.reject((error, data) => {
-  if (error) {
-    console.log('error')
-    return true
-  } else {
-    return false
-  }
-})
-.map((error, data) => data.toString())
-.bind((prevdata, next) => {
-  fs.readFile('test/test.js', (error, data) => {
-    next(prevdata + data.toString())
+new IO(callback => fs.readFile('test/test.js', callback))
+  .reject((error, data) => {
+    if (error) {
+      console.log('error')
+      return true
+    } else {
+      return false
+    }
   })
-})
-.next(data => console.log(data))
+  .map((error, data) => data.toString())
+  .bind((prevdata, next) => {
+    fs.readFile('test/test.js', (error, data) => {
+      next(prevdata + data.toString())
+    })
+  })
+  .then(data => console.log(data))
