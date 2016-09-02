5336fed46e075af7e89f3a0cd9ce55fe7a83552b
Santosh Rajan <santrajan@gmail.com>
2016-09-02 17:48:53 +0530

    refactors IO

--- test/test.js
+++ test/test.js
@@ -3,18 +3,7 @@ const IO = require('../lib/io-square')
 const fs = require('fs')
 
 new IO(callback => fs.readFile('test/test.js', callback))
-  .reject((error, data) => {
-    if (error) {
-      console.log('error')
-      return true
-    } else {
-      return false
-    }
-  })
-  .map((error, data) => data.toString())
-  .bind((prevdata, next) => {
-    fs.readFile('test/test.js', (error, data) => {
-      next(prevdata + data.toString())
-    })
-  })
-  .then(data => console.log(data))
+  .reject((error, data) => error ? null : [data])
+  .map(data => [data.toString()])
+  .bind(new IO(cb => fs.readFile('test/test.js', cb)))
+  .then((data, error, newData) => console.log(data + newData.toString()))
