5eb20f369f0898e8bf9db0b0e604793a29864f89
Santosh Rajan <santrajan@gmail.com>
2016-09-17 11:25:27 +0530

    adds clean

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,12 @@
 import gulp from 'gulp'
 import babel from 'gulp-babel'
+import del from 'del'
 
 gulp.task('compile-lib', () =>
   gulp.src('src/**/*.js')
   .pipe(babel())
   .pipe(gulp.dest('lib')))
+
+gulp.task('clean', () => del(['lib']))
 
 gulp.task('default', ['compile-lib'])
